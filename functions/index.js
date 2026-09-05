c878b192d53e32dcc4441d1e86d419580209438c
Derrick Kwaku Adonteng <138382322+dkadonteng-developer@users.noreply.github.com>
2026-09-05 13:41:41 +0000

    Change default value of 'gl' parameter from 'us' to 'gh'

--- functions/index.js
+++ functions/index.js
@@ -64,7 +64,7 @@ exports.sourceParts = onRequest(
       engine: 'google_shopping',
       q: query,
       api_key: apiKey,
-      gl: (typeof gl === 'string' && gl) || 'us',
+      gl: (typeof gl === 'string' && gl) || 'gh',
       hl: 'en',
     });
 
